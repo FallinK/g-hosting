fd297b66934a15faf9692f113d4676485a637fee
Kirill Kalachev <kalachev.kirill@bk.ru>
2022-08-19 19:23:38 +0300

    Add about-us section and peculiarities section

--- js/script.js
+++ js/script.js
@@ -3,15 +3,19 @@
 const menuLinks = document.querySelectorAll('.menu__link[data-goto]');
 if (menuLinks.length > 0) {
 	menuLinks.forEach( menuLink => {
-			menuLink.addEventListener("click", onMenuLinkClick);
+			menuLink.addEventListener('click', onMenuLinkClick);
 		});
-
 		function onMenuLinkClick (e) {
 			const menuLink = e.target;
 			if (menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)) {
 				const gotoBlock = document.querySelector(menuLink.dataset.goto);
 				const gotoBlockValue = gotoBlock.getBoundingClientRect().top + scrollY - document.querySelector('header').offsetHeight;
 				
+				if(burger.classList.contains('_active')) {
+					document.body.classList.remove('_lock');
+					burger.classList.remove('_active');
+					menuBody.classList.remove('_active');
+				}
 				window.scrollTo ({
 					top: gotoBlockValue,
 					behavior: "smooth"
@@ -19,4 +23,15 @@ if (menuLinks.length > 0) {
 				e.preventDefault();
 			}
 		}
-	}
+}
+//BURGER MENU
+
+const burger = document.querySelector("div[class='menu__icon']");
+const menuBody = document.querySelector("div[class='menu__body']");
+if(burger) {
+	burger.addEventListener('click', function(){
+		document.body.classList.toggle('_lock');
+		burger.classList.toggle('_active');
+		menuBody.classList.toggle('_active');
+	});
+}
